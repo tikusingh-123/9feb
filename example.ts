ac1df7dfb9185aa6743ed7db133c8009b721bf3f
tikusingh-123 <75766866+tikusingh-123@users.noreply.github.com>
2021-02-09 20:13:08 +0530

    Create example.ts

---
+++ example.ts
@@ -0,0 +1,22 @@
+<div class="container fluid">
+    <div style="height: 100px ; background-color: orange">
+    <img  class="mw-100 mh-25   " src="https://wallpaperaccess.com/full/15818.jpg" alt="...">
+</div>
+<div class="card">
+    
+    <h5 class="card-header">welcome</h5>
+
+
+
+    <div class="card-body">
+        <img src="https://www.w3schools.com/bootstrap/cinqueterre.jpg" width="25%" class="rounded-circle float-right" alt="Cinque Terre">
+
+      <h5 class="card-title">name</h5>
+      
+      <p class="card-text text-muted">member since</p>
+    
+    </div>
+  </div>
+
+</div>
+
